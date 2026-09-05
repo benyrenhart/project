68565322743898520d0703ed8734ae7d4bd7e473
benyrenhart <76201585+benyrenhart@users.noreply.github.com>
2026-09-05 22:17:13 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = {
+  plugins: [require("flowbite/plugin")],
+};
+module.exports = {
+  content: ["./node_modules/flowbite/**/*.js"],
+};
